47260313d042024832df913edac6f302a12232d3
NioZow <niozow@proton.me>
2024-03-25 14:05:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Searchbar from "@/components/elements/searchbar"
+
+const App = () => {
+  return <div className="max-w-4xl flex item-center justify-center w-full h-full"><Searchbar /></div>
+}
+
+export default App
