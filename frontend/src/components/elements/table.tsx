8ca2c2bad55066aeba7e0a31778923f05f1d8edb
NioZow <niozow@gmail.com>
2024-04-02 23:17:23 +0200

    table almost done (just read thing to do)

--- frontend/src/components/elements/table.tsx
+++ frontend/src/components/elements/table.tsx
@@ -42,7 +42,7 @@ import { Input } from "@/components/ui/input";
 import { Training } from "@/utils/types";
 import { RenderIcon } from "@/components/icons/icons";
 
-import { sortTrainings, FilterTrainings } from "@/utils/training.utils";
+import { getTrainings } from "@/utils/training.utils";
 
 import { capitalizeFirstLetter } from "@/utils/utils";
 
@@ -82,17 +82,37 @@ const TrainingTable = () => {
     Actions: getStateFromLocalStorage("Actions"),
   });
 
-  const [filterValue, setFilterValue] = useState("");
-  const [filterColumn, setFilterColumn] = useState<column>("Name");
+  const [page, setPage] = useState<number>(0);
+  const [pageSize, setPageSize] = useState<number>(
+    Number(window.localStorage.getItem("pageSize")) !== 0
+      ? Number(window.localStorage.getItem("pageSize"))
+      : 20
+  );
+
+  const [filterValue, setFilterValue] = useState<string>(
+    window.localStorage.getItem("filter") !== null
+      ? (window.localStorage.getItem("filter") as string)
+      : ""
+  );
+  const [filterColumn, setFilterColumn] = useState<column>(
+    window.localStorage.getItem("column_filter") !== null
+      ? (window.localStorage.getItem("column_filter") as column)
+      : "Name"
+  );
+
   const [sortColumn, setSortColumn] = useState<column>("Name");
   const [sortAsc, setSortAsc] = useState(true);
   const [openColumn, setColumnOpen] = useState(false);
   const [dropboxHumanClosed, setDropboxHumanClosed] = useState(false);
 
-  const trainingsSorted: Array<Training> = FilterTrainings(
+  let trainingsFiltered = getTrainings(
     trainings,
+    page,
+    pageSize,
     filterColumn,
-    filterValue
+    filterValue,
+    sortColumn,
+    sortAsc
   );
 
   return (
@@ -101,14 +121,23 @@ const TrainingTable = () => {
         <Input
           placeholder={`Filter by ${filterColumn.toLowerCase()}...`}
           value={filterValue ?? ""}
-          onChange={(event) => setFilterValue(event.target.value)}
+          onChange={(event) => {
+            setFilterValue(event.target.value);
+            window.localStorage.setItem("filter", event.target.value);
+          }}
           className="max-w-sm"
         />
 
         <Select
           onValueChange={(event) => {
             setFilterColumn(event as column);
+            window.localStorage.setItem("column_filter", event);
           }}
+          defaultValue={
+            window.localStorage.getItem("column_filter") !== null
+              ? (window.localStorage.getItem("column_filter") as column)
+              : ""
+          }
         >
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Select a filter" />
@@ -178,7 +207,6 @@ const TrainingTable = () => {
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -215,122 +243,115 @@ const TrainingTable = () => {
           </TableHeader>
 
           <TableBody>
-            {trainingsSorted.length != 0 ? (
-              sortTrainings(trainingsSorted, sortColumn, sortAsc).map(
-                (training, i) => {
-                  return (
-                    <TableRow key={i}>
+            {trainingsFiltered.length != 0 ? (
+              trainingsFiltered.map((training, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell>
+                      <RenderIcon icon={training.Tags} />
+                    </TableCell>
+
+                    {columns["Type"] ? (
+                      <TableCell>{training.Type}</TableCell>
+                    ) : null}
+
+                    {columns["Name"] ? (
+                      <TableCell>{training.Name}</TableCell>
+                    ) : null}
+
+                    {columns["Tags"] ? (
                       <TableCell>
-                        <RenderIcon icon={training.Tags} />
+                        {training.Tags.map((tag, i) => {
+                          return i + 1 != training.Tags.length
+                            ? `${tag} - `
+                            : `${tag}`;
+                        })}
                       </TableCell>
+                    ) : null}
 
-                      {columns["Type"] ? (
-                        <TableCell>{training.Type}</TableCell>
-                      ) : null}
-
-                      {columns["Name"] ? (
-                        <TableCell>{training.Name}</TableCell>
-                      ) : null}
-
-                      {columns["Tags"] ? (
-                        <TableCell>
-                          {training.Tags.map((tag, i) => {
-                            return i + 1 != training.Tags.length
-                              ? `${tag} - `
-                              : `${tag}`;
-                          })}
-                        </TableCell>
-                      ) : null}
-
-                      {columns["Authors"] ? (
-                        <TableCell>
-                          {training.Authors.map((author, i) => {
-                            return i + 1 != training.Authors.length
-                              ? `${author} - `
-                              : `${author}`;
-                          })}
-                        </TableCell>
-                      ) : null}
-
-                      {columns["Date"] ? (
-                        <TableCell>{training.Date}</TableCell>
-                      ) : null}
-
-                      {columns["Time to read"] ? (
-                        <TableCell>{training.Time}</TableCell>
-                      ) : null}
-
-                      {columns["Difficulty"] ? (
-                        <TableCell>
-                          {capitalizeFirstLetter(training.Difficulty)}
-                        </TableCell>
-                      ) : null}
-
-                      {columns["Price"] ? (
-                        <TableCell>{training.Price}</TableCell>
-                      ) : null}
-
-                      {columns["Read"] ? (
-                        <TableCell>
-                          {window.localStorage.getItem(training.Url) === null
-                            ? "No"
-                            : "Yes"}
-                        </TableCell>
-                      ) : null}
-
+                    {columns["Authors"] ? (
                       <TableCell>
-                        <DropdownMenu>
-                          <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-8 w-8 p-0">
-                              <span className="sr-only">Open menu</span>
-                              <DotsHorizontalIcon className="h-4 w-4" />
-                            </Button>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                            <DropdownMenuItem
-                              onClick={() =>
-                                navigator.clipboard.writeText(training.Url)
-                              }
-                            >
-                              Copy training url
-                            </DropdownMenuItem>
-                            <DropdownMenuItem
-                              onClick={() =>
-                                window.open(training.Url, "_blank")
-                              }
-                            >
-                              Open training in new tab
-                            </DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            {window.localStorage.getItem(training.Url) ===
-                            null ? (
-                              <DropdownMenuItem
-                                onClick={() => {
-                                  window.localStorage.setItem(
-                                    training.Url,
-                                    "a"
-                                  );
-                                }}
-                              >
-                                Mark as read
-                              </DropdownMenuItem>
-                            ) : (
-                              <DropdownMenuItem
-                                onClick={() => {
-                                  window.localStorage.removeItem(training.Url);
-                                }}
-                              >
-                                Mark as not read
-                              </DropdownMenuItem>
-                            )}
-                          </DropdownMenuContent>
-                        </DropdownMenu>
+                        {training.Authors.map((author, i) => {
+                          return i + 1 != training.Authors.length
+                            ? `${author} - `
+                            : `${author}`;
+                        })}
                       </TableCell>
-                    </TableRow>
-                  );
-                }
-              )
+                    ) : null}
+
+                    {columns["Date"] ? (
+                      <TableCell>{training.Date}</TableCell>
+                    ) : null}
+
+                    {columns["Time to read"] ? (
+                      <TableCell>{training.Time}</TableCell>
+                    ) : null}
+
+                    {columns["Difficulty"] ? (
+                      <TableCell>
+                        {capitalizeFirstLetter(training.Difficulty)}
+                      </TableCell>
+                    ) : null}
+
+                    {columns["Price"] ? (
+                      <TableCell>{training.Price}</TableCell>
+                    ) : null}
+
+                    {columns["Read"] ? (
+                      <TableCell>
+                        {window.localStorage.getItem(training.Url) === null
+                          ? "No"
+                          : "Yes"}
+                      </TableCell>
+                    ) : null}
+
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <DotsHorizontalIcon className="h-4 w-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem
+                            onClick={() =>
+                              navigator.clipboard.writeText(training.Url)
+                            }
+                          >
+                            Copy training url
+                          </DropdownMenuItem>
+                          <DropdownMenuItem
+                            onClick={() => window.open(training.Url, "_blank")}
+                          >
+                            Open training in new tab
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          {window.localStorage.getItem(training.Url) ===
+                          null ? (
+                            <DropdownMenuItem
+                              onClick={() => {
+                                window.localStorage.setItem(training.Url, "a");
+                              }}
+                            >
+                              Mark as read
+                            </DropdownMenuItem>
+                          ) : (
+                            <DropdownMenuItem
+                              onClick={() => {
+                                window.localStorage.removeItem(training.Url);
+                              }}
+                            >
+                              Mark as not read
+                            </DropdownMenuItem>
+                          )}
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                );
+              })
             ) : (
               <TableRow>
                 <TableCell
@@ -344,33 +365,47 @@ const TrainingTable = () => {
           </TableBody>
         </Table>
       </div>
-    </div>
-  );
-};
-
-/*
-
       <div className="flex items-center justify-end space-x-2 py-4">
+        <Input
+          placeholder={`Number of trainings per page (${pageSize})`}
+          onChange={(event) => {
+            let input = Number(event.target.value);
+
+            if (event.target.value === "") {
+              setPageSize(20);
+              window.localStorage.setItem("pageSize", "20");
+            } else if (!isNaN(input)) {
+              setPageSize(input);
+              window.localStorage.setItem("pageSize", event.target.value);
+            }
+          }}
+          className="max-w-sm"
+        />
         <div className="space-x-2">
           <Button
             variant="outline"
             size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
+            onClick={() => {
+              setPage(page - 1);
+            }}
+            disabled={page === 0}
           >
             Previous
           </Button>
           <Button
             variant="outline"
             size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
+            onClick={() => {
+              setPage(page + 1);
+            }}
+            disabled={page >= Math.floor(trainings.length / 20)}
           >
             Next
           </Button>
         </div>
       </div>
-
-*/
+    </div>
+  );
+};
 
 export default TrainingTable;
--- frontend/src/utils/training.utils.ts
+++ frontend/src/utils/training.utils.ts
@@ -6,94 +6,20 @@ import {
   columnArray,
 } from "@/utils/types";
 
-export function sortTrainings(
-  trainings: Array<Training>,
-  column: column,
-  sortAsc: boolean
-): Array<Training> {
-  let trainingsSorted: Array<Training> = [];
-
-  // not true columns, do not sort using those
-  if (column == "" || column == "Actions" || column == "Read") {
-    return trainings;
-  }
-
-  if (["Name", "Type", "Read", "Difficulty"].includes(column)) {
-    if (sortAsc) {
-      trainings
-        .sort((a, b) =>
-          a[column as columnString].localeCompare(b[column as columnString])
-        )
-        .map((training) => {
-          trainingsSorted.push(training);
-        });
-    } else {
-      // reverse order
-      trainings
-        .sort((a, b) =>
-          b[column as columnString].localeCompare(a[column as columnString])
-        )
-        .map((training) => {
-          trainingsSorted.push(training);
-        });
-    }
-  } else if (["Time to read"].includes(column)) {
-    if (sortAsc) {
-      trainings
-        .sort((a, b) => a.Time - b.Time)
-        .map((training) => {
-          trainingsSorted.push(training);
-        });
-    } else {
-      trainings
-        .sort((a, b) => b.Time - a.Time)
-        .map((training) => {
-          trainingsSorted.push(training);
-        });
-    }
-  } else if (["Price"].includes(column)) {
-    if (sortAsc) {
-      trainings
-        .sort((a, b) => a[column as columnNumber] - b[column as columnNumber])
-        .map((training) => {
-          trainingsSorted.push(training);
-        });
-    } else {
-      trainings
-        .sort((a, b) => a[column as columnNumber] - b[column as columnNumber])
-        .map((training) => {
-          trainingsSorted.push(training);
-        });
-    }
-  } else if (["Authors", "Tags"].includes(column)) {
-    // convert to an array of strings
-    let array: Array<Array<string>> = [];
-    trainings.map((training) => {
-      array.push([training[column as columnArray].toString(), training.Name]);
-    });
-
-    // sort this one
-    array
-      .sort((a, b) => a[0].localeCompare(b[0]))
-      .map((training) => {
-        let value: Training | null = GetTrainingByName(trainings, training[1]);
-
-        if (value != null) {
-          trainingsSorted.push(value as Training);
-        }
-      });
-
-    if (!sortAsc) {
-      trainingsSorted.reverse();
-    }
-  } else {
-    trainingsSorted = trainings;
-  }
-
-  return trainingsSorted;
-}
-
-const GetTrainingByName = (
+/*
+ * @brief
+ *  get a training by its name
+ *
+ * @param trainings
+ *  trainings
+ *
+ * @param name
+ *  name of the training to get
+ *
+ * @return
+ *  training
+ */
+const getTrainingByName = (
   trainings: Array<Training>,
   name: string
 ): Training | null => {
@@ -106,9 +32,114 @@ const GetTrainingByName = (
   return null;
 };
 
-export const FilterTrainings = (
+/*!
+ * @brief
+ *  sort trainings
+ *
+ * @param trainings
+ *  trainings to sort
+ *
+ * @param column
+ *  column to sort by
+ *
+ * @param sortAsc
+ *  sort ascending or descending
+ *
+ * @return
+ *  array of trainings
+ */
+const sortTrainings = (
   trainings: Array<Training>,
-  column: string,
+  column: column,
+  sortAsc: boolean
+): Array<Training> => {
+  let trainingsSorted: Array<Training> = [];
+
+  // not true columns, do not sort using those
+  if (column == "" || column == "Actions") {
+    return trainings;
+  }
+
+  if (column === "Read") {
+    trainings.map((training) => {
+      if (window.localStorage.getItem(training.Url) !== null) {
+        trainingsSorted.push(training);
+      }
+    });
+
+    trainings.map((training) => {
+      if (window.localStorage.getItem(training.Url) === null) {
+        trainingsSorted.push(training);
+      }
+    });
+  } else if (["Name", "Type", "Read", "Difficulty"].includes(column)) {
+    trainings
+      .sort((a, b) =>
+        a[column as columnString].localeCompare(b[column as columnString])
+      )
+      .map((training) => {
+        trainingsSorted.push(training);
+      });
+  } else if (["Time to read"].includes(column)) {
+    trainings
+      .sort((a, b) => a.Time - b.Time)
+      .map((training) => {
+        trainingsSorted.push(training);
+      });
+  } else if (["Price"].includes(column)) {
+    trainings
+      .sort((a, b) => a[column as columnNumber] - b[column as columnNumber])
+      .map((training) => {
+        trainingsSorted.push(training);
+      });
+  } else if (["Authors", "Tags"].includes(column)) {
+    // convert to an array of strings
+    let array: Array<Array<string>> = [];
+    trainings.map((training) => {
+      array.push([training[column as columnArray].toString(), training.Name]);
+    });
+
+    // sort this one
+    array
+      .sort((a, b) => a[0].localeCompare(b[0]))
+      .map((training) => {
+        let value: Training | null = getTrainingByName(trainings, training[1]);
+
+        if (value != null) {
+          trainingsSorted.push(value as Training);
+        }
+      });
+  } else {
+    trainingsSorted = trainings;
+  }
+
+  // reverse it if its descending
+  if (!sortAsc) {
+    trainingsSorted.reverse();
+  }
+
+  return trainingsSorted;
+};
+
+/*!
+ * @brief
+ *  filter trainings by a keyword
+ *
+ * @param trainings
+ *  trainings to filter
+ *
+ * @param column
+ *  column to filter
+ *
+ * @param value
+ *  value to use as a filter
+ *
+ * @return
+ *  array of trainings
+ */
+const filterTrainings = (
+  trainings: Array<Training>,
+  column: column,
   value: string
 ): Array<Training> => {
   if (value === "") {
@@ -118,6 +149,7 @@ export const FilterTrainings = (
   let trainingsFiltered: Array<Training> = [];
 
   if (["Name", "Type", "Read", "Difficulty"].includes(column)) {
+    // filter
     trainings.map((training) => {
       if (
         training[column as columnString]
@@ -128,18 +160,21 @@ export const FilterTrainings = (
       }
     });
   } else if (["Time to read"].includes(column)) {
+    // filter
     trainings.map((training) => {
       if (training.Time <= parseInt(value, 10)) {
         trainingsFiltered.push(training);
       }
     });
   } else if (["Price"].includes(column)) {
+    // filter
     trainings.map((training) => {
       if (training.Price <= parseInt(value, 10)) {
         trainingsFiltered.push(training);
       }
     });
   } else if (["Authors", "Tags"].includes(column)) {
+    // filter
     trainings.map((training) => {
       let elements: string = "";
 
@@ -161,4 +196,70 @@ export const FilterTrainings = (
   }
 
   return trainingsFiltered;
+};
+
+/*!
+ * @brief
+ *  get only a limited of trainings (use of pages)
+ *
+ * @param trainings
+ *  trainings to limit
+ *
+ * @param page
+ *  page to get the training
+ *
+ * @param pageSize
+ *  size of the pages
+ *
+ * @return
+ *  array of trainings of maximum size: pageSize
+ */
+const limitTrainings = (
+  trainings: Training[],
+  page: number,
+  pageSize: number
+): Training[] => {
+  let trainingsLimited: Training[] = [];
+  let numberOfTrainingsOnThisPage =
+    page * 20 + pageSize > trainings.length
+      ? trainings.length
+      : page * 20 + pageSize;
+
+  for (let i = page * 20; i < numberOfTrainingsOnThisPage; i++) {
+    trainingsLimited.push(trainings[i]);
+  }
+
+  return trainingsLimited;
+};
+
+/*!
+ * @brief
+ *  sort, filter, and limit the number of trainings shown on the page
+ *
+ * @param trainings
+ *  trainings to filter, sort and limit
+ *
+ * @param
+ *
+ */
+export const getTrainings = (
+  trainings: Training[],
+  page: number,
+  pageSize: number,
+  columnFilter: column,
+  filter: string,
+  columnSort: column,
+  sortAsc: boolean
+): Training[] => {
+  console.log("realod");
+
+  return limitTrainings(
+    sortTrainings(
+      filterTrainings(trainings, columnFilter, filter),
+      columnSort,
+      sortAsc
+    ),
+    page,
+    pageSize
+  );
 };
